33e810218b632427d7884aeed66c5638817335b4
Piers Ebdon <piers@peapps.co.uk>
2024-10-11 17:56:24 +0100

    make use of requirements file in package setup

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
+from setuptools import find_packages
 from setuptools import setup
+
+with open("requirements.txt") as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content if "git+" not in x]
 
 setup(name="shroom-ai",
       description="Package for identifyng edible mushroom via an image",
-      packages=["shroom-ai"])
+      requires=requirements,
+      packages=find_packages())
