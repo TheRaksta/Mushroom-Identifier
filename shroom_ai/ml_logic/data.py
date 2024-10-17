b08753a6c7e43741ce76ea43f5d31ec755abc4f3
Piers Ebdon <piers@peapps.co.uk>
2024-10-17 15:38:58 +0100

    handle uploading resized images to gcp_bucket

--- shroom_ai/ml_logic/data.py
+++ shroom_ai/ml_logic/data.py
@@ -2,7 +2,7 @@
 from google.cloud import storage
 from io import BytesIO
 from PIL import Image
-from shroom_ai.params import ORIGINAL_KAGGLE_IMAGES_GCP_BUCKET_NAME
+from shroom_ai.params import ORIGINAL_KAGGLE_IMAGES_GCP_BUCKET_NAME, RESIZED_KAGGLE_IMAGES_GCP_BUCKET_NAME
 from typing import List
 
 def get_blobs_from_bucket(bucket_name: str) -> List[storage.Blob]:
@@ -16,19 +16,24 @@ def resize_image(image_data, new_size=(224, 224), format="PNG"):
     img = img.resize(new_size)
     buffer = BytesIO()
     img.save(fp=buffer, format=format)
+    ## returns image data
     return buffer.getvalue()
 
+def upload_resized_images(source_bucket_name: str, dest_bucket_name: str, new_size=(224, 224), format="PNG", upload_content_type="image/png"):
+    blobs = get_blobs_from_bucket(source_bucket_name)
 
+    storage_client = storage.Client()
+    dest_bucket = storage_client.bucket(dest_bucket_name)
 
+    for blob in blobs:
+        image_data = blob.download_as_bytes()
+        resized_image_data = resize_image(image_data=image_data, new_size=new_size, format=format)
+        new_blob = dest_bucket.blob(blob.name)
+        new_blob.upload_from_string(resized_image_data, content_type=upload_content_type)
+        print(f'Resized and uploaded: {blob.name} to {dest_bucket_name}')
+
+    print(" Finished uploading all the magic 🍄🍄🍄🍄🍄🍄🍄🍄")
 
 ## Will not be committed
 if __name__ == "__main__":
-    bucket_name = ORIGINAL_KAGGLE_IMAGES_GCP_BUCKET_NAME
-    blobs = get_blobs_from_bucket(bucket_name)
-    for blob in blobs:
-        print(blob.name)
-        image_data = blob.download_as_bytes()
-        print(type(image_data))
-        resized_image_data = resize_image(image_data=image_data)
-        print("Resized", type(resized_image_data))
-        break
+    upload_resized_images(source_bucket_name=ORIGINAL_KAGGLE_IMAGES_GCP_BUCKET_NAME, dest_bucket_name=RESIZED_KAGGLE_IMAGES_GCP_BUCKET_NAME)
