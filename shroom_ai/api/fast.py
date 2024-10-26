49c4dbb65fdeb10633ddf1703b719f83110a0423
Piers Ebdon <piers@peapps.co.uk>
2024-10-26 11:45:08 +0100

    get endpoint up that accepts an image

--- shroom_ai/api/fast.py
+++ shroom_ai/api/fast.py
@@ -1,1 +1,30 @@
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from PIL import Image
+from model_deployment.utils.image_utils import preprocess_image
+import io
 
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.post("/predict")
+async def upload_image(file: UploadFile = File(...)):
+    # Check if the file is an image
+    if file.content_type not in ["image/jpeg", "image/png"]:
+        raise HTTPException(status_code=400, detail="Invalid file type. Only JPEG or PNG images are supported.")
+
+    # Read the image content
+    contents = await file.read()
+    image = Image.open(io.BytesIO(contents))
+
+    # turn image to numpy array
+    numpy_array = preprocess_image(image)
+
+    # fetch model
+
+
+    # run prediction
+
+    # pass reponse
--- shroom_ai/model_deployment/utils/image_utils.py
+++ shroom_ai/model_deployment/utils/image_utils.py
@@ -1,9 +1,9 @@
 from PIL import Image
 import numpy as np
-from config import config
+from model_deployment.config.config import IMAGE_SIZE
 
 def preprocess_image(image):
     img = Image.open(image).convert('RGB')
-    img = img.resize(config.IMAGE_SIZE)
+    img = img.resize(IMAGE_SIZE)
     img_array = np.array(img) / 255.0
     return np.expand_dims(img_array, axis=0)
